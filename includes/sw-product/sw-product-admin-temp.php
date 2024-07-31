26ef93906b17a2b42a4e5cdfc1ae3c9b513151c4
Callismart Tech <admin@callismart.com.ng>
2024-07-31 14:02:21 +0100

    Update sw-product-admin-temp.php
    Added frontend view url for products

--- includes/sw-product/sw-product-admin-temp.php
+++ includes/sw-product/sw-product-admin-temp.php
@@ -46,6 +46,7 @@ function smartwoo_product_table() {
 		$page_html .= '<td>' . esc_html( $product->get_billing_cycle() ) . '</td>';
 		$page_html .= '<td>';
 		$page_html .= '<a href="' . esc_url( admin_url( 'admin.php?page=sw-products&action=edit&product_id=' . $product->get_id() ) ) . '" class="button">Edit</a>';
+		$page_html .= '<a href="' . esc_url( get_permalink( $product->get_id() ) ) . '" class="button">View</a>';
 		$page_html .= '<button class="sw-delete-product" data-product-id="'. esc_attr( $product->get_id() ) . '">' . __( 'Delete', 'smart-woo-service-invoicing' ) . '</button>';
 		$page_html .= '</td>';
 		$page_html .= '</tr>';
