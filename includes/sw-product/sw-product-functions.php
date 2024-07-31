bd0fc139ff76fb1387eacb3e1c86338f2a73d377
Callismart Tech <admin@callismart.com.ng>
2024-07-31 14:01:48 +0100

    Update sw-product-functions.php
    Implemented admin product page url function

--- includes/sw-product/sw-product-functions.php
+++ includes/sw-product/sw-product-functions.php
@@ -48,5 +48,30 @@ function smartwoo_delete_product() {
 
 function smartwoo_product_text_on_shop() {	
 	$pruduct_text = get_option( 'smartwoo_product_text_on_shop', 'Configure' );
-	return $pruduct_text;
+	return apply_filters( 'smartwoo_product_text_on_shop', $pruduct_text );
+}
+
+/**
+ * Get url to the admin product page endpoints.
+ * 
+ * @param $action The action that determines which page to load.
+ * @param $product_id	The ID of product the action is being performed on.
+ * @return string $page	URL of the matching page, defaults to the product dashboard.
+ * @since 2.0.0
+ */
+function smartwoo_admin_product_url( $action = '', $product_id = 0 ) {
+	$allowed_actions 	= array( 'add-new', 'edit' );
+	$page				= admin_url( 'admin.php?page=sw-products' );
+
+	if ( ! in_array( $action, $allowed_actions, true ) ) {
+		return $page;
+	}
+
+	if ( 'add-new' === $action ) {
+		$page = $page. '&action=' . $action;
+	} elseif ( 'edit' === $action && ! empty( $product_id ) ) {
+		$page = $page. '&action=' . $action . '&product_id=' . $product_id;
+	}
+
+	return $page;
 }
