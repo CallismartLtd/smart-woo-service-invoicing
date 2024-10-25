9528f19d43b899b771c4c2946af99cfcf4f60f23
Callismart Tech <admin@callismart.com.ng>
2024-10-25 11:46:11 +0100

    Update smart-woo-service-invoicing.php

--- smart-woo-service-invoicing.php
+++ smart-woo-service-invoicing.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Smart Woo Service Invoicing
  * Description: More than just a subscription plugin.
- * Version: 2.0.14
+ * Version: 2.0.15
  * Author: Callistus Nwachukwu
  * Author URI: https://callismart.com.ng/callistus
  * Plugin URI: https://callismart.com.ng/smart-woo-service-invoicing
@@ -44,11 +44,11 @@ if ( ! defined( 'SMARTWOO_DIR_URL' ) ) {
 
 // Define the Smart Woo versions.
 if ( ! defined( 'SMARTWOO_VER' ) ) {
-	define( 'SMARTWOO_VER', '2.0.14' );
+	define( 'SMARTWOO_VER', '2.0.15' );
 }
 
 if ( ! defined( 'SMARTWOO_DB_VER' ) ) {
-	define( 'SMARTWOO_DB_VER', '2.0.14' );
+	define( 'SMARTWOO_DB_VER', '2.0.15' );
 }
 
 // Load core and config files.
