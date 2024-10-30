8164c7ce69d5bc645555f514822dede2b3a49324
Callismart Tech <admin@callismart.com.ng>
2024-10-30 09:10:42 +0100

    Update template.php

--- includes/frontend/service/template.php
+++ includes/frontend/service/template.php
@@ -173,7 +173,7 @@ function smartwoo_service_front_temp() {
 	} else {
 		$buy_product_page = smartwoo_service_page_url() . 'buy-new/';
 		$output       .= '<div class="main-page-card">';
-		$output       .= '<p>All Service will appear here</p>';
+		$output       .= '<p>No service found.</p>';
 		$output       .= '<a href="' . esc_url( $buy_product_page ) . '" class="sw-blue-button">' . esc_html__( 'Buy New Service', 'smart-woo-service-invoicing' ) . '</a>';
 		$output .= '</div>';
 	}
