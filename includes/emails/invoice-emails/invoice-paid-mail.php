298c35026cd6fbd1865f5c2794621245832a5e90
Callismart Tech <admin@callismart.com.ng>
2024-11-30 10:03:33 +0100

    Update invoice-paid-mail.php

--- includes/emails/invoice-emails/invoice-paid-mail.php
+++ includes/emails/invoice-emails/invoice-paid-mail.php
@@ -19,12 +19,20 @@ class SmartWoo_Invoice_Paid_Mail extends SmartWoo_Invoice_Mails {
     protected $invoice;
 
     /**
+     * Static self
+     * 
+     * @var SmartWoo_Invoice_Paid_Mail $instance
+     */
+    public static $instance = null;
+
+    /**
      * Class constructor
      */
     public function __construct( $invoice ) {
         $this->invoice = $invoice;
 
-        parent::__construct( 'Invoice Payment Confirmation', $this->get_template(), $invoice );
+        parent::__construct( 'Invoice Payment Confirmation', self::get_template(), $invoice );
+        self::$instance = $this;
 
     }
 
@@ -49,7 +57,7 @@ class SmartWoo_Invoice_Paid_Mail extends SmartWoo_Invoice_Mails {
     /**
      * Default email template
      */
-    public function get_template() {
+    public static function get_template() {
         $message  = '<h1>Payment Receipt "{{invoice_id}}"</h1>';
 		$message .= '<p>Dear <strong>{{client_fullname}}</strong>,</p>';
 		$message .= '<p>This is a payment receipt for invoice #{{invoice_id}} paid on {{invoice_date_paid}}.</p>';
@@ -61,33 +69,10 @@ class SmartWoo_Invoice_Paid_Mail extends SmartWoo_Invoice_Mails {
         $message .= '<li>Total: {{invoice_total}}</li>';
         $message .= '</ul>';
 		
-        $template = apply_filters( 'smartwoo_invoice_paid_mail_template', $message, $this );
+        $template = apply_filters( 'smartwoo_invoice_paid_mail_template', $message, self::$instance );
         return $template;
     }
 
-        /**
-     * Email preview buffer
-     */
-    public static function start_preview_buffer() {
-        
-        $invoice    = new SmartWoo_Invoice();
-      
-        $invoice->set_invoice_id( smartwoo_generate_invoice_id() );
-        $invoice->set_user_id( get_current_user_id() );
-        $invoice->set_product_id( self::get_random_product_id() );
-        $invoice->set_amount( wp_rand( 200, 500 ) );
-        $invoice->set_total( wp_rand( 200, 500 ) );
-        $invoice->set_status( 'unpaid' );
-        $invoice->set_date_created( 'now' );
-        $invoice->set_billing_address( smartwoo_get_client_billing_email( get_current_user_id() ) );
-        $invoice->set_date_paid( 'now' );
-        $invoice->set_service_id( smartwoo_generate_service_id( 'Awesome Service' ) );
-        $invoice->set_type( 'Billing' );
-        $invoice->set_fee( wp_rand( 200, 500 ));
-        $invoice->set_date_due( 'now' );
-        $self   = new self( $invoice );
-        $self->preview_template();
-    }
 }
 
 SmartWoo_Invoice_Paid_Mail::init();
