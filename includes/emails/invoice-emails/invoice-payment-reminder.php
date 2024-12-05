e1d518ba93dc0c8e2db56fe15b7b6ec545f88def
Callismart Tech <admin@callismart.com.ng>
2024-12-05 22:21:06 +0100

    Update invoice-payment-reminder.php

--- includes/emails/invoice-emails/invoice-payment-reminder.php
+++ includes/emails/invoice-emails/invoice-payment-reminder.php
@@ -10,6 +10,12 @@
 defined( 'ABSPATH' ) || exit;
 
 class SmartWoo_Invoice_Payment_Reminder extends SmartWoo_Invoice_Mails {
+    /**
+     * Email ID
+     * 
+     * @var string $id
+     */
+    public static $id = 'smartwoo_payment_reminder_to_client';
 
     /**
      * The invoice
@@ -23,7 +29,7 @@ class SmartWoo_Invoice_Payment_Reminder extends SmartWoo_Invoice_Mails {
      * 
      * @var SmartWoo_Invoice_Payment_Reminder $instance
      */
-    public static $instance = null;
+    public static $instance = 'SmartWoo_Invoice_Payment_Reminder';
 
     /**
      * Class constructor
@@ -60,18 +66,24 @@ class SmartWoo_Invoice_Payment_Reminder extends SmartWoo_Invoice_Mails {
         $message .= '<p>Dear <strong>{{client_fullname}}</strong>,</p>';
         $message .= '<p>We hope this email finds you well. We would like to bring to your attention an outstanding invoice associated with your account.</p>';
         
-        $message .= '<p>Invoice Details:</p>';
+        $message .= '<br><h3>Invoice Details</h3>';
+        $message .= '<ul>';
+        $message .= '<li>Invoice ID: <strong>{{invoice_id}}</strong></li>';
+        $message .= '</ul>';
+        $message .= '{{invoice_items}}';
+        $message .= '<p style="text-align:right; margin-right:10%;"><strong>Total</strong>: {{invoice_total}}</p>';
         $message .= '<ul>';
         $message .= '<li>Balance Due: {{invoice_total}}</li>';
         $message .= '<li>Due Date: {{invoice_date_due}}</li>';
-        $message .= '</ul>';
+        $message .= '</ul><hr>';
         $message .= '<p>To make the payment securely, please click the button below:</p>';
         $message .= '<p><a class="button" href="{{auto_login_payment_link}}">Pay Now</a></p>';
         $message .= '<p>If the button above does not work, you may use the following link:</p>';
         $message .= '<a href="{{auto_login_payment_link}}">{{auto_login_payment_link}}</a>';
         $message .= '<p>Please note: This link will expire in 24 hours. After that, you may need to log into your account manually to make the payment.</p>';
+		$message .= '<p><strong>View invoice:</strong> <a href="{{preview_url}}">{{preview_url}}</a></p>';
 
-        $template = apply_filters( 'smartwoo_new_invoice_mail_template', $message, self::$instance );
+        $template = apply_filters( 'smartwoo_payment_reminder_to_client_template', $message, self::$instance );
         return $template;
     }
 }
