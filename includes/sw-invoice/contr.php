7d7813eeaa7c87dfff84bc8d67b15295fcd75232
Callismart Tech <admin@callismart.com.ng>
2025-02-08 11:23:56 +0100

    Update contr.php

--- includes/sw-invoice/contr.php
+++ includes/sw-invoice/contr.php
@@ -18,6 +18,26 @@ class SmartWoo_Invoice_Form_Controller{
 	 * @var SmartWoo_Invoice_Form_Controller
 	 */
 	private static $instance = null;
+
+	/**
+	 * Stores form fields.
+	 * 
+	 * @var array
+	 */
+	private $form_fields = array(
+		'invoice_id'		=> '',
+		'product_id'		=> 0,
+		'fee'				=> 0,
+		'service_id'		=> '',
+		'user_id'			=> 0,
+		'user_email'		=> '',
+		'due_date'			=> 'now',
+		'invoice_type'		=> '',
+		'payment_status' 	=> 'unpaid'
+		
+	);
+
+
 	/**
 	 * Class constructor
 	 */
@@ -28,6 +48,8 @@ class SmartWoo_Invoice_Form_Controller{
 
 	/**
 	 * Singleton instance of current class.
+	 * 
+	 * @return self
 	 */
 	public static function instance() {
 		if ( is_null( self::$instance ) ) {
@@ -50,8 +72,114 @@ class SmartWoo_Invoice_Form_Controller{
 		if ( ! isset( $_POST['smartwoo_send_new_invoice_mail'] ) || 'yes' !== $_POST['smartwoo_send_new_invoice_mail'] ) {
 			remove_action( 'smartwoo_new_invoice_created', array( 'SmartWoo_New_Invoice_Mail', 'send_mail' ) );
 		}
-		// Validate inputs.
-		$errors = array();        
+		
+		$errors = apply_filters( 'smartwoo_invoice_form_error', self::instance()->check_errors() );
+
+		if ( ! empty( $errors ) ) {
+			wp_send_json_error( array( 'htmlContent' => smartwoo_error_notice( $errors, true ) ), 30 );
+		}
+
+		$is_guest_invoice = isset( $_POST['is_guest_invoice'] ) && 'yes' === $_POST['is_guest_invoice'];
+
+		$invoice = self::create_invoice( $is_guest_invoice );
+
+		if ( is_wp_error( $invoice ) ) {
+			wp_send_json_error( array( 'message' => $invoice->get_error_message(), 'redirect_url' => admin_url( 'admin.php?page=sw-invoices&tab=add-new-invoice' ) ) );
+		} else{
+			do_action( 'smartwoo_handling_new_invoice_form_success', $invoice );
+			wp_send_json_success( array( 'message' => 'Invoice Created', 'redirect_url' => $invoice->preview_url( 'admin' ) ) );
+		}
+
+        
+    }
+
+
+	/**
+	 * Helper method to create invoice.
+	 * 
+	 * @param bool $is_guest_invoice Whether the invoice is a guest invoice, defaults to false.
+	 * @return SmartWoo_Invoice|WP_Error SmartWoo_Invoice object when the submision is valid, WP_Error otherwise
+	 */
+	private static function create_invoice( $is_guest_invoice = false ) {
+		$args 		= self::instance()->get_form_data();
+		$amount		= wc_get_product( $args['product_id'] )->get_price();
+		$total		= $amount + $args['fee'];
+		$invoice	= new SmartWoo_Invoice();
+
+		$invoice->set_invoice_id( smartwoo_generate_invoice_id() );
+		$invoice->set_product_id( $args['product_id'] );
+		$invoice->set_amount( $amount );
+		$invoice->set_total( $total );
+		$invoice->set_status( $args['payment_status'] );
+		$invoice->set_date_created( current_time( 'mysql' ) );
+		$invoice->set_user_id( $args['user_id'] );
+		$invoice->set_type( $args['invoice_type'] );
+		$invoice->set_service_id( $args['service_id'] );
+		$invoice->set_fee( $args['fee'] );
+		$invoice->set_date_due( $args['due_date'] );
+
+		if ( 'paid' === $args['payment_status'] ) {
+			$invoice->set_date_paid( 'now' );
+		}
+
+		if ( $is_guest_invoice ) {
+			$guest_data = self::instance()->get_posted_guest_data();
+			foreach( $guest_data as $key => $value ) {
+				$invoice->set_meta( $key, $value );
+			}
+		} else {
+			// Billing Address is typically set for guests
+			$invoice->set_billing_address( smartwoo_get_client_billing_email( $args['user_id'] ) );
+
+		}
+
+		if ( 'unpaid' === $args['payment_status'] ) {
+			$invoice->save(); // Persist changes before order creation.
+			$order_id = smartwoo_generate_pending_order( $args['user_id'], $invoice->get_invoice_id() );
+			$invoice->set_order_id( $order_id );
+		}
+
+		return $invoice->save() ? $invoice: new WP_Error( 'invoice_creation_error', 'Invoice creation failed', array( 'status' => 503 ) );
+
+	}
+
+	/**
+	 * Get the submitted form data.
+	 * 
+	 * @return array
+	 */
+	private function get_form_data() {
+		return $this->form_fields;
+	}
+
+	/**
+	 * The the posted guest data
+	 * 
+	 * @return array
+	 */
+	private function get_posted_guest_data() {
+		$guest_data = array(
+			'first_name'		=> isset( $_POST['first_name'] ) ? sanitize_text_field( wp_unslash( $_POST['first_name'] ) ) : '',
+			'last_name'			=> isset( $_POST['last_name'] ) ? sanitize_text_field( wp_unslash( $_POST['last_name'] ) ) : '',
+			'billing_address'	=> isset( $_POST['billing_address'] ) ? sanitize_text_field( wp_unslash( $_POST['billing_address'] ) ) : '',
+			'phone'				=> isset( $_POST['phone'] ) ? sanitize_text_field( wp_unslash( $_POST['phone'] ) ) : '',
+			'billing_company'	=> isset( $_POST['billing_company'] ) ? sanitize_text_field( wp_unslash( $_POST['billing_company'] ) ) : '',
+			'billing_email'		=> isset( $_POST['billing_email'] ) ? sanitize_text_field( wp_unslash( $_POST['billing_email'] ) ) : '',
+		);
+
+		return $guest_data;
+	}
+
+	/**
+	 * Form error checker
+	 * 
+	 * @return array|false An array of errors or false when no error is found.
+	 */
+	public function check_errors() {
+		$errors = array();
+		/**
+		 * Check user data.
+		 */
 		$user_data		= isset( $_POST['user_data'] ) ? sanitize_text_field( wp_unslash( $_POST['user_data'] ) ) : '';
 		if ( ! $user_data ) {
 			$error[] = 'Please select a user.';
@@ -71,17 +199,46 @@ class SmartWoo_Invoice_Form_Controller{
 		if ( ! $user_email || ! is_email( $user_email ) ) {
 			$errors[] = 'The user\'s email is not valid.';
 		}
-
-		$product_id     = isset( $_POST['product_id'] ) ? intval( $_POST['product_id'] ) : 0;
-		$invoice_type	= ! empty( $_POST['invoice_type'] ) ? sanitize_text_field( wp_unslash( $_POST['invoice_type'] ) ) : 'Billing';
-		$service_id     = isset( $_POST['service_id'] ) ? sanitize_text_field( wp_unslash( $_POST['service_id'] ) ) : '';
-		$due_date       = isset( $_POST['due_date'] ) ? sanitize_text_field( wp_unslash( $_POST['due_date'] ) ) : current_time( 'mysql' );				
-		$fee            = isset( $_POST['fee'] ) ? floatval( $_POST['fee'] ) : 0;
-		$payment_status = isset( $_POST['payment_status'] ) ? sanitize_text_field( wp_unslash( $_POST['payment_status'] ) ) : 'unpaid';
 		
-		// Check for a duplicate unpaid invoice for a service.
-		$invoice_type_exists = false;
+		if ( empty( $user_id ) ) {
+			$errors[] = 'The user does not have a valid ID.';
+		}
 
+		$this->form_fields['user_id'] 		= $user_id;
+		$this->form_fields['user_email']	= $user_email;
+
+		/**
+		 * Check product data.
+		 */
+		$product_id	= isset( $_POST['product_id'] ) ? intval( $_POST['product_id'] ) : 0;
+		if ( empty( $product_id ) && ! SmartWoo::pro_is_installed() ) {
+			$errors[] = 'Add a product to the invoice.';
+		}
+
+		$product = wc_get_product( $product_id );
+		if ( ! $product || ! is_a( $product, 'SmartWoo_Product') ) {
+			$errors[] = 'The selected product does not exist.';
+		}
+
+		$this->form_fields['product_id'] = $product_id;
+
+		/**
+		 * Check Service ID field for possible duplicate invoice type for a service.
+		 */
+		$service_id	= isset( $_POST['service_id'] ) ? sanitize_text_field( wp_unslash( $_POST['service_id'] ) ) : '';
+		$this->form_fields['service_id'] = $service_id;
+
+		/**
+		 * Check invoice Type
+		 */
+		$invoice_type	= ! empty( $_POST['invoice_type'] ) ? sanitize_text_field( wp_unslash( $_POST['invoice_type'] ) ) : 'Billing';
+		if ( empty( $invoice_type ) ) {
+			$errors[] = 'Please select a valid Invoice Type.';
+		}
+		$this->form_fields['invoice_type'] = $invoice_type;
+
+		$invoice_type_exists 	= false;
+		
 		if ( ! empty( $service_id ) ) {
 			$invoice_type_exists = smartwoo_evaluate_service_invoices( $service_id, $invoice_type, 'unpaid' );
 		}
@@ -90,58 +247,16 @@ class SmartWoo_Invoice_Form_Controller{
 			$errors[] = 'This Service has "' . $invoice_type . '" that is ' . $payment_status;
 		}
 
-		if ( empty( $user_id ) ) {
-			$errors[] = 'The user does not have a valid ID.';
-		}
+		$due_date	= isset( $_POST['due_date'] ) ? sanitize_text_field( wp_unslash( $_POST['due_date'] ) ) : current_time( 'mysql' );				
+		$this->form_fields['due_date'] = $due_date;
+		
+		$fee	= isset( $_POST['fee'] ) ? floatval( $_POST['fee'] ) : 0;
+		$this->form_fields['fee'] = $fee;
 
-		if ( empty( $product_id ) && ! SmartWoo::pro_is_installed() ) {
-			$errors[] = 'Add a product to the invoice.';
-		}
+		$payment_status = isset( $_POST['payment_status'] ) ? sanitize_text_field( wp_unslash( $_POST['payment_status'] ) ) : 'unpaid';
+		$this->form_fields['payment_status'] = $payment_status;
 
-		if ( empty( $invoice_type ) ) {
-			$errors[] = 'Please select a valid Invoice Type.';
-		}
-
-		$errors = apply_filters( 'smartwoo_handling_new_invoice_form_error', $errors );
-
-		if ( ! empty( $errors ) ) {
-			smartwoo_set_form_error( $errors );
-			wp_send_json_error( array( 'htmlContent' => smartwoo_error_notice( $errors, true ) ), 30 );
-		}
-
-		$createdInvoiceID = self::create_invoice( compact( 'user_id', 'product_id', 'payment_status', 'invoice_type', 'service_id', 'fee', 'due_date' ) );
-
-		if ( $createdInvoiceID ) {
-			do_action( 'smartwoo_handling_new_invoice_form_success', $createdInvoiceID );
-			$detailsPageURL = esc_url( admin_url( "admin.php?page=sw-invoices&tab=view-invoice&invoice_id=$createdInvoiceID" ) );
-			wp_send_json_success( array( 'redirect_url' => $detailsPageURL ) );
-		}
-		smartwoo_set_form_error( 'Something went wrong.' );
-		wp_send_json_error( array( 'redirect_url' => admin_url( 'admin.php?page=sw-invoices&tab=add-new-invoice' ) ) );
-		 
-        
-    }
-
-
-	/**
-	 * Helper method to create invoice.
-	 * 
-	 * @param array $args
-	 * @return true|WP_Error True when the submision is valid, WP_Error otherwise
-	 */
-	private static function create_invoice( array $args ) {
-		$default_args = array(
-			'user_id'		=> -1, // Guest.
-			'product_id'	=> 0,
-			'status'		=> 'unpaid',
-			'invoice_type'	=> '',
-			'service_id'	=> '',
-			'fee'			=> 0,
-			'due_date'		=> current_time( 'mysql' )
-		);
-
-		$parsed_args = wp_parse_args( $args, $default_args );
-		$invoice		= new SmartWoo_Invoice();
+		return ( ! empty( $errors ) ) ? $errors : false;
 	}
 }
 
