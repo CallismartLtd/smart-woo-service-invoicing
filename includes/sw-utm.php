f0addd69d29c03518009c84bed0b0db9aaeee9f5
Callismart Tech <admin@callismart.com.ng>
2025-02-13 06:34:39 +0100

    Update sw-utm.php

--- includes/sw-utm.php
+++ includes/sw-utm.php
@@ -183,3 +183,15 @@ function smartwoo_help_container( $echo = true) {
 		return $content;
 	}
 }
+
+/**
+ * Function to return pro feature template file.
+ */
+function smartwoo_pro_feature_template( $feature = '' ) {
+    add_filter( 'smartwoo_pro_template_arg', function() use ( $feature ) {
+       return $feature;
+   
+    } );
+
+    return SMARTWOO_PATH . 'templates/pro-feature.php';
+}
