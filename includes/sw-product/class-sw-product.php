1ec4bf451723056f2265d1d53b49b0a8fbb69843
Callismart Tech <admin@callismart.com.ng>
2025-02-18 10:42:17 +0100

    Update class-sw-product.php

--- includes/sw-product/class-sw-product.php
+++ includes/sw-product/class-sw-product.php
@@ -504,7 +504,7 @@ class SmartWoo_Product extends WC_Product {
 			$billing_cycle = $product->get_billing_cycle();
 	
 			$notice_banner  = '<div class="smartwoo-sub-info">';
-			$notice_banner .= '<p class="main-price"> You will be charged  <strong>' . smartwoo_price( $product->get_price() ) . ' ' . esc_html( ucfirst( $billing_cycle ) ) . '</strong></p>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			$notice_banner .= '<p class="main-price"><strong>' . smartwoo_price( $product->get_price() ) . '</strong> Billed <strong>' . esc_html( ucfirst( $billing_cycle ) ) . '</strong></p>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	
 			if ( $sign_up_fee > 0 ) {
 				$notice_banner .=  '<p class="sign-up-fee">and a one-time sign-up fee of <strong>' . smartwoo_price( $sign_up_fee ) . '</strong></p>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
