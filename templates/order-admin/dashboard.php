1a5792fe02f00804d52f8a2cff8f652f67a86f87
Callismart Tech <admin@callismart.com.ng>
2025-02-25 19:28:46 +0100

    Create dashboard.php

---
+++ templates/order-admin/dashboard.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The order management dashboard template file
+ * 
+ * @author Callistus
+ * @package SmartWoo\admin\templates
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<h1 class="wp-heading-inline">Service Orders</h1>
+<?php if ( empty( $orders ) ): ?>
+    <?php echo wp_kses_post( smartwoo_notice( 'All Service orders will appear here when a customer purchases a service product.' ) ); ?>
+<?php endif; ?>
+
+<table class="sw-table">
+    <thead>
+        <tr>
+        <th><input type="checkbox" name="" id="swTableCheckMaster"></th>
+        <th>Order ID</th>
+        <th>Date Created</th>
+        <th>Status</th>
+        <th>Service Name</th>
+        <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach( $orders as $order ): ?>
+            <tr>
+                <td><input type="checkbox" data="<?php echo absint( $order->get_id() );?>" class="sw-table-body-checkbox"></td>
+                <td><?php echo esc_html( $order->get_order_id() . ' | ' . $order->get_id() ); ?></td>
+                <td><?php echo esc_html( $order->get_date_created( 'plain' ) ); ?></td>
+                <td><?php echo esc_html( $order->get_status() ); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
