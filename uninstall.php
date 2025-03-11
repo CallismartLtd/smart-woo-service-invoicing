48f60b4e742cff8fef5ce6263c93d37c0d82a2ff
Callismart Tech <admin@callismart.com.ng>
2025-03-11 16:01:07 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -69,6 +69,7 @@ $options = array(
     'smartwoo_allow_invoice_tracking',
     '_smartwoo_flushed_rewrite_rules',
     '__smartwoo_automation_last_scheduled_date',
+    'smartwoo_allow_fast_checkout'
 );
 
 foreach ( $options as $option ) {
