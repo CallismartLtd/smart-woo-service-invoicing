c398d75aee53ea77cb7fad75cf5b6c0f8776252a
Callismart Tech <admin@callismart.com.ng>
2025-03-17 17:37:21 +0100

    Update stats.php

--- templates/service-admin-temp/stats.php
+++ templates/service-admin-temp/stats.php
@@ -9,9 +9,11 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <div class="sw-admin-view-details">
     <?php echo wp_kses_post( smartwoo_sub_menu_nav( $tabs, 'Usage Metrics','sw-admin', $tab, 'service_id=' . $service_id . '&tab' ) ); ?>
-    <?php if( class_exists( 'SmartWooPro', false ) && method_exists( 'SmartWooPro', 'load_usage' ) ) : ?>
-        <?php call_user_func_array( array( new SmartWooPro(), 'load_usage' ), array( '', $service_id, true ) ) ?>
-    <?php else: ?>
-        <?php echo wp_kses_post( smartwoo_pro_feature( 'advanced stats' ) ); ?>
-    <?php endif; ?>
+    <div class="wrap">
+        <?php if( class_exists( 'SmartWooPro', false ) && method_exists( 'SmartWooPro', 'load_usage' ) ) : ?>
+            <?php call_user_func_array( array( new SmartWooPro(), 'load_usage' ), array( '', $service_id, true ) ) ?>
+        <?php else: ?>
+            <?php echo wp_kses_post( smartwoo_pro_feature( 'advanced stats' ) ); ?>
+        <?php endif; ?>
+    </div>
 </div>
