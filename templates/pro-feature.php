d8d758a8a678d6d80575c1d028c531b87e5e20ea
Callismart Tech <admin@callismart.com.ng>
2025-05-22 19:10:41 +0100

    Update pro-feature.php

--- templates/pro-feature.php
+++ templates/pro-feature.php
@@ -7,5 +7,5 @@
  */
 
 defined( 'ABSPATH' ) || exit;
-$arg = apply_filters( 'smartwoo_pro_template_arg', '' );
-echo wp_kses_post( smartwoo_pro_feature( $arg ) );
+
+echo wp_kses_post( smartwoo_pro_feature( $feature ) );
