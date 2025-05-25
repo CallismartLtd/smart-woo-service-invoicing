575cf3a299b27652543dd0f9d1bca09c5b6be9d8
Callismart Tech <admin@callismart.com.ng>
2025-05-25 21:17:33 +0100

    Update my-account.php

--- includes/frontend/woocommerce/my-account.php
+++ includes/frontend/woocommerce/my-account.php
@@ -12,28 +12,6 @@
 defined( 'ABSPATH' ) || exit; // Prevent direct access.
 
 /**
- * Callback function for plugin WooCommerce account menu.
- */
-function smartwoo_register_woocommerce_account_menu( $items ) {
-	
-    $new_items = array(
-        'smartwoo-invoice' => __( 'Invoices', 'smart-woo-service-invoicing' ),
-        'smartwoo-service' => __( 'Services', 'smart-woo-service-invoicing' ),
-    );
-
-    $position_invoice = 3;
-    $position_service = 4;
-
-    $items = array_slice( $items, 0, $position_invoice, true ) +
-        $new_items +
-        array_slice( $items, $position_invoice, NULL, true ) +
-    array_slice( $items, $position_service, NULL, true );
-			 
-	return $items;
-}
-
-
-/**
  * Content callback for my Invoice account menu item.
  */
 function smartwoo_invoice_myaccount_content() {
@@ -45,30 +23,5 @@ function smartwoo_invoice_myaccount_content() {
         $invoice_content .= smartwoo_get_unpaid_invoices_count();
         $invoice_content .= smartwoo_invoice_mini_card();
         echo wp_kses_post( $invoice_content );
-    }
-}
-
-// Service Page Content
-function smartwoo_service_myaccount_content() {
-    if ( isset( $_GET['view_service'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-        echo  wp_kses_post( smartwoo_service_details() ) ;
-    } else {
-        $service_content = '<div class="smartwoo-page">';
-        $service_content .= '<h2>' . __( 'Services', 'smart-woo-service-invoicing' ). '</h2>';
-        $service_content .= smartwoo_active_service_count_shortcode();
-        $service_content .= smartwoo_service_mini_card();
-        $service_content .= '<div class="settings-tools-section">';
-        $service_content .= '<h2>Settings and Tools</h2>';
-        $service_content .= '<div id="swloader">Just a moment</div>';
-        $service_content .= '<div class="sw-button-container">';
-        $service_content .= '<a class="sw-blue-button" id="sw-billing-details">Billing Details</a>';
-        $service_content .= '<a class="sw-blue-button" id="sw-load-user-details">My Details</a>';
-        $service_content .= '<a class="sw-blue-button" id="sw-account-log">Account Logs</a>';
-        $service_content .= '<a class="sw-blue-button" id="sw-load-transaction-history">Transaction History</a>';
-        $service_content .= '</div>';
-        $service_content .= '<div id="ajax-content-container"></div>'; //Respone container.
-        $service_content .= '</div>';
-        $service_content .= '</div>';
-        echo wp_kses_post( $service_content );
     }
 }
