c66e4498f951d3437a4353ffe1d3d6f960149a23
Callismart Tech <admin@callismart.com.ng>
2025-05-27 21:40:22 +0100

    Update front.php

--- templates/frontend/subscriptions/front.php
+++ templates/frontend/subscriptions/front.php
@@ -10,14 +10,17 @@ defined( 'ABSPATH' ) || exit; ?>
 
 <div class="smartwoo-page">
     <?php echo wp_kses_post( smartwoo_get_navbar( 'My Services' ) ); ?>
+    <span style="float:right;"><?php smartwoo_table_limit_field( $limit ); ?></span>
     <p style="text-align: center; margin-top: 10px;">Welcome, <?php echo esc_html( $full_name ); ?>!</p>
-    <div class="status-counts"> 
+    
+    <div class="status-counts portal"> 
         <p class="active-count"><a href="<?php echo esc_url( $active_count_url ); ?>">Active: <?php echo esc_html( $active_count ); ?></a></p>
 		<p class="due-for-renewal-count"><a href="<?php echo esc_url( $due_count_url ); ?>">Due: <?php echo esc_html( $due_for_renewal_count ); ?></a></p>
 	    <p class="expired-count"><a href="<?php echo esc_url( $expired_count_url ); ?>">Expired: <?php echo esc_html( $expired_count ); ?></a></p>
 	    <p class="grace-period-count"><a href="<?php echo esc_url( $grace_count_url ); ?>">Grace Period: <?php echo esc_html( $grace_period_count ); ?></a></p>
 	</div>
 
+    
     <div class="client-services">
         <?php if ( empty( $services ) ) : ?>
             <div class="main-page-card">
@@ -25,12 +28,15 @@ defined( 'ABSPATH' ) || exit; ?>
                 <a href="<?php echo esc_url( $buy_product_page ); ?>" class="sw-blue-button"><?php esc_html_e( 'Buy New Service', 'smart-woo-service-invoicing' ); ?></a>
             </div>
         <?php else: ?>
+            
             <?php foreach ( $services as $service ) : ?>
                 <div class="main-page-card">
                     <h3><?php echo esc_html( $service->get_name() ); ?> <small>(<?php echo esc_html( smartwoo_service_status( $service ) ); ?>)</small></h3>
                     <?php echo wp_kses_post( $service->print_expiry_notice() ); ?>
-                    <p style="overflow-wrap: anywhere;">Service ID: <?php echo esc_html( $service->get_service_id() ); ?></p>
-                    <a href="<?php echo esc_url( smartwoo_service_preview_url( $service->get_service_id() ) ); ?>" class="sw-blue-button"><?php esc_html_e( 'View Details', 'smart-woo-service-invoicing' ); ?></a>
+                    <?php if( 'Processing' !== smartwoo_service_status( $service ) ) : ?>
+                        <p style="overflow-wrap: anywhere;">Service ID: <?php echo esc_html( $service->get_service_id() ); ?></p>
+                        <a href="<?php echo esc_url( smartwoo_service_preview_url( $service->get_service_id() ) ); ?>" class="sw-blue-button"><?php esc_html_e( 'View Details', 'smart-woo-service-invoicing' ); ?></a>
+                <?php endif; ?>
                 </div>
             <?php endforeach; ?>
         <?php endif; ?>
