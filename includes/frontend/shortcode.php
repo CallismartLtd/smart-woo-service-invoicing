9ab11ab8ce8f685d9dc5c46e2fc25898976e75c9
Callismart Tech <admin@callismart.com.ng>
2025-05-27 01:48:24 +0100

    Update shortcode.php

--- includes/frontend/shortcode.php
+++ includes/frontend/shortcode.php
@@ -13,22 +13,15 @@
 function smartwoo_shortcodes_init() {
 
 	/**
-	 * Short code for invoice page, should be on
-	 * the page where you choose as invoice page
+	 * Short code for invoice page.
 	 */
 
-	add_shortcode( 'smartwoo_invoice_page', 'smartwoo_invoice_shortcode' );
-
-	/**
-	 * Short code for Service Subscription page, should be on
-	 * the page where you choose as Service page
-	 */
-	add_shortcode( 'smartwoo_service_page', array( 'SmartWoo_Frontend_Template', 'shortcode_handler' ) );
+	add_shortcode( 'smartwoo_invoice_page', array( 'SmartWoo_Invoice_Frontend_Template', 'shortcode_handler' ) );
 
 	/**
-	 * Display an integer value of unpaid invoices
+	 * Short code for Service Subscription page.
 	 */
-	add_shortcode( 'smartwoo_unpaid_invoices_count', 'smartwoo_get_unpaid_invoices_count' );
+	add_shortcode( 'smartwoo_service_page', array( 'SmartWoo_Service_Frontend_Template', 'shortcode_handler' ) );
 
 	/**
 	 * Display the service subscriptions of the current user in a mini card
@@ -48,14 +41,6 @@ function smartwoo_shortcodes_init() {
 	add_shortcode( 'smartwoo_invoice_status_counts', 'smartwoo_all_user_invoices_count' );
 
 	/**
-	 * This line is for All transactions shortcodes
-	 */
-
-	add_shortcode( 'smartwoo_pending_transactions_count', 'smartwoo_get_pending_transactions_count' );
-	add_shortcode( 'smartwoo_transactions', 'smartwoo_transactions_shortcode' );
-	add_shortcode( 'smartwoo_transaction_status', 'smartwoo_transaction_status_shortcode' );
-
-	/**
 	 * @since 2.0.14 Added new shortcode for login form
 	 */
 	add_shortcode( 'smartwoo_login_form', 'smartwoo_render_login_form' );
@@ -67,22 +52,23 @@ add_action( 'init', 'smartwoo_shortcodes_init' );
  * Callback function for login form shortcode.
  */
 function smartwoo_render_login_form( $atts ) {
-	$atts = shortcode_atts( array(
-		'notice'		=> '',                     
-		'redirect_url' 	=> get_permalink(),
-	), $atts, 'smartwoo_login_form' );
+	$atts = shortcode_atts( 
+		array(
+			'notice'		=> '',                     
+			'redirect_url' 	=> get_permalink(),
+		),
+		$atts, 
+		'smartwoo_login_form'
+	);
 
-	// Form is hidded when user is logged in
 	if ( is_user_logged_in() ) {
 		return '';
 	}
 
-	// Prepare options for the login form
 	$options = array(
 		'notice'   => ! empty( $atts['notice'] ) ? smartwoo_notice( esc_html( $atts['notice'] ) ): esc_html( $atts['notice'] ),
 		'redirect' => esc_url( $atts['redirect_url'] ),
 	);
 
-	// Return the login form with the provided options
 	return smartwoo_login_form( $options );
 }
