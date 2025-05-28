6bfc3c845da7507bdd8226b6e8c4c5f3f854b364
Callismart Tech <admin@callismart.com.ng>
2025-05-28 15:08:31 +0100

    Update smartwoo-products-catalog.php

--- templates/frontend/smartwoo-products-catalog.php
+++ templates/frontend/smartwoo-products-catalog.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit; ?>
     <?php echo wp_kses_post( smartwoo_get_navbar( 'Buy New Service', get_permalink( wc_get_page_id( 'shop' ) ) ) ); ?>
     <?php if ( empty( $smartwoo_products ) ) : ?>
         <div class="main-page-card">
-            <p><?php esc_html_e( 'No subscripton product found, please contact us if you help.', 'smart-woo-service-invoing' ) ?></p>
+            <p><?php esc_html_e( 'No subscripton product found, please contact us if you help.', 'smart-woo-service-invoicing' ); ?></p>
             <a href="<?php echo esc_url( $shop_page_url ); ?>" class="sw-blue-button"><?php echo esc_html__( 'Shop Page', 'smart-woo-service-invoicing' ); ?></a>
             <a href="<?php echo esc_attr( get_permalink() ); ?>" class="sw-blue-button"><?php echo esc_html__( 'Dashboard', 'smart-woo-service-invoicing' ); ?></a>
         </div>
@@ -22,8 +22,8 @@ defined( 'ABSPATH' ) || exit; ?>
                 <div class="sw-product-container">
                     <img src="<?php echo esc_url( $product->get_image_id() ? wp_get_attachment_url( $product->get_image_id() ) : wc_placeholder_img_src() ); ?>" alt="<?php echo esc_attr( $product->get_name() ); ?> image">
                     <h3><?php echo esc_html( $product->get_name() ); ?></h3>
-                	<p>Price: <?php echo smartwoo_price( $product->get_regular_price() ); ?></p>
-                    <p>Sign-Up Fee: <?php echo smartwoo_price( $product->get_sign_up_fee() ); ?></p>
+                	<p>Price: <?php echo esc_html( smartwoo_price( $product->get_regular_price() ) ); ?></p>
+                    <p>Sign-Up Fee: <?php echo esc_html( smartwoo_price( $product->get_sign_up_fee() ) ); ?></p>
                     <p>Billed: <strong><?php echo esc_html( $product->get_billing_cycle() ); ?></strong></p>
                     <div class="sw-description">
                         <?php echo wp_kses_post( $product->get_short_description() ); ?>
