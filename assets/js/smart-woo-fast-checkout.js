2bb595608143f76a2ccb94acb645be919d989e22
Callismart Tech <admin@callismart.com.ng>
2025-05-31 00:00:05 +0100

    Update smart-woo-fast-checkout.js

--- assets/js/smart-woo-fast-checkout.js
+++ assets/js/smart-woo-fast-checkout.js
@@ -17,108 +17,131 @@ function smartwooFastCheckout() {
  * @param {Event} event The event listener obj
  */
 function smartwooOpenModal( event ) {
-    let productID   = event.target.getAttribute( 'data-product_id' );
+    let productLi       = event.target.closest( 'li' );
+    let fsConfig        = smart_woo_vars.fast_checkout_config;
 
-    // If no product ID, search within the closest product container
-    if ( ! productID ) {
-        let productContainer = event.target.closest('[data-product_id]');
-        if ( productContainer ) {
-            productID = productContainer.dataset.product_id;
-        }
+    if ( ! productLi ) {
+        productLi   = event.target.closest( '.configure-product-button' );
     }
 
-    // If product ID is still missing, exit
-    if ( ! productID ) {
-        console.warn("Product ID not found. Fast checkout prevented.");
+    let configHolder    = productLi.querySelector( '.smartwoo-product-data' );
+
+    if ( ! configHolder ) {
+        console.warn( "Product config not found. Fast checkout prevented." );
         return;
     }
-
     event.preventDefault();
-    let modalRemove = ( element )=>{
-        element.classList.remove( 'active' );
-        setTimeout(() => {
-            element.remove();
-        }, 1000);
-    }
+
+    let configData      = configHolder.getAttribute( 'smartwoo-product-config' );
+    let product         = JSON.parse( configData );
+    let productID       = product.id;
 
     let prevDiv = document.querySelector( '.sw-fast-checkout-main' );
-
-    if ( prevDiv ) {
-        modalRemove( prevDiv );
+    
+    let modalRemove = ()=>{
+        modal = document.querySelector( '.smartwoo-modal-frame' );
+        jQuery( modal ).fadeOut( 'fast', () =>{
+            modal.remove();
+        });
     }
 
+    if ( prevDiv ) {
+        modalRemove();
+    }
+
+    let modalFrame  = document.createElement( 'div' );
     let modalDiv    = document.createElement( 'div' );
     let closeBtn    = document.createElement( 'span' );
-    let hTag    = document.createElement( 'h3' );
+    let hTag        = document.createElement( 'h3' );
     let inputDiv    = document.createElement( 'div' );
 
-    hTag.textContent = `Configure ${event.target.getAttribute( 'data-product_name' ) ? `"${event.target.getAttribute( 'data-product_name' )}"` : ''}`;
+    modalFrame.classList.add( 'smartwoo-modal-frame' );
+    modalDiv.setAttribute( 'style', `background-color: ${fsConfig.modal_background_color};` );
+    modalFrame.appendChild( modalDiv );
+
+    let titleText = fsConfig.title.replace('{{product_name}}', `"${product.name}"` || '');
+    hTag.textContent = titleText;
+    hTag.setAttribute( 'style', `padding: 5px; color: ${fsConfig.title_color}`)
     hTag.style.padding = '5px';
     let fields       = `
         <div id="response"></div>
-        <input type="text" name="service_name" id="service_name" placeholder="Service Name (required)"/>
-        <input type="text" name="service_url" id="service_url" placeholder="URL (optional)"/>
-        <button class="sw-blue-button">Checkout</button>
-        <div id="loader" style="background-color:rgba(255, 255, 255, 0);"></div>
+        <div class="smartwoo-fast-checkout-description">
+            <img src="${product.image_url}" alt="${product.name}">
+            <span>${fsConfig.description}</span>
+        </div>
+        <input type="text" name="service_name" id="service_name" placeholder="${fsConfig.service_name_placeholder}"/>
+        <input type="text" name="service_url" id="service_url" placeholder="${fsConfig.url_placeholder}"/>
+        <button class="sw-blue-button" style="background-color: ${fsConfig.button_background_color}; color: ${fsConfig.button_text_color};">${fsConfig.checkout_button_text}</button>
+        <div id="loader" style="background-color:rgba(255, 255, 255, 0); text-align: center; display: none;"></div>
 
     `;
-    closeBtn.setAttribute( 'style', 'height: 20px; float: right; text-align: right; color: red; margin-right: 10px; cursor: pointer; font-weight: 900;');
-    closeBtn.textContent = 'x';
+    closeBtn.setAttribute( 'style', 'float: right; color:rgb(248, 100, 100); margin-right: 4px; cursor: pointer; font-size: 24px');
+    closeBtn.setAttribute( 'class', 'dashicons dashicons-dismiss' );
+    // closeBtn.innerHTML = '&times;';
     modalDiv.classList.add( 'sw-fast-checkout-main' );
     
     inputDiv.classList.add( 'sw-flex-column' );
     inputDiv.style.position = 'relative';
     inputDiv.innerHTML = fields;
-    modalDiv.appendChild(closeBtn);
+    hTag.appendChild(closeBtn);
     modalDiv.appendChild(hTag);
     modalDiv.appendChild(inputDiv);
-    document.body.prepend( modalDiv );
-    setTimeout( () => {
-        modalDiv.classList.add( 'active' );
+    document.body.prepend( modalFrame );
+    jQuery( modalFrame ).fadeIn( 'slow' );
 
-    }, 50);
     closeBtn.addEventListener( 'click', () =>{
-        modalRemove( modalDiv );
-        
+        modalRemove();        
+    });
+    
+    modalFrame.addEventListener( 'click', (e) =>{
+        if ( e.target === modalFrame ) {
+            modalRemove();
+        }
     });
 
     let removeModal = ( e ) => {
         if ( e.key === 'Escape' || e.key === 'Esc') {
-            modalRemove( modalDiv );
+            modalRemove();
+            document.removeEventListener( 'keydown', removeModal );
         }
-
-        document.removeEventListener( 'keydown', removeModal );
-
     }
+
     document.addEventListener( 'keydown', removeModal );
 
-    checkoutBtn = modalDiv.querySelector( 'button' );
+    let responseDiv = modalDiv.querySelector( '#response' );
+    responseDiv.style.border = "solid 1px #dcdcde";
+    responseDiv.style.padding = "5px";
+    responseDiv.style.minHeight = "40px";
+    responseDiv.style.display = "none";
+
+    let checkoutBtn = modalDiv.querySelector( 'button' );
+    let spinner;
     checkoutBtn.addEventListener( 'click', e =>{
+        jQuery( responseDiv ).fadeOut();
+        checkoutBtn.setAttribute( 'disabled', true );
         let serviceName = modalDiv.querySelector( '#service_name' );
         let serviceURL  = modalDiv.querySelector( '#service_url' );
-        let responseDiv = modalDiv.querySelector( '#response' );
-        responseDiv.style.border = "solid 1px #dcdcde";
-        responseDiv.style.padding = "5px";
-        responseDiv.style.minHeight = "40px";
-
-        let clearForm = () =>{
-            responseDiv.innerHTML = '';
-            responseDiv.style.border = "none";
-        }
 
         if ( ! ( serviceName.value.trim() ).length ) {
             responseDiv.innerHTML = "Please enter a service name";
+            jQuery( responseDiv ).fadeIn();
+            checkoutBtn.removeAttribute( 'disabled' );
             return;
         }
-        clearForm();
+
         let animateBg = ( element ) => {
-            let colors = [' #ffffff', '#e2def5', '#b3afaf', '#e0f6ff'];
+            let colors = [' #ffffff', ' #e2def5','rgb(129, 122, 122)', ' #b3afaf', 'rgb(61, 196, 250)'];
             return setInterval(() => {
                 element.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
             }, 200);
         };
+
+        if ( ! spinner ) {
+            spinner     = smartWooAddSpinner( 'loader', true );  
+        }
+
+        jQuery( spinner ).show();
         
-        let spinner     = smartWooAddSpinner( 'loader', true );
         let animation   = animateBg(modalDiv);
         spinner.style.position = 'absolute';
         spinner.style.top = '20%';
@@ -149,18 +172,18 @@ function smartwooOpenModal( event ) {
                 responseDiv.style.border = "solid 1px #dcdcde";
                 responseDiv.innerHTML = responseData.data.message ? responseData.data.message : 'Product configured, redirecting to checkout.';
                 window.location.href = responseData.data.checkout;
+                jQuery( responseDiv ).show();
             } else {
-                responseDiv.style.border = "solid 1px #dcdcde";
                 responseDiv.innerHTML = responseData.data.message ? responseData.data.message : 'Something went wrong.';
-                
+                jQuery( responseDiv ).show();
             }
         }).catch( error =>{
             console.error( error );
+            jQuery( responseDiv ).show();
         }).finally( ()=>{
-            smartWooRemoveSpinner( spinner );
+            jQuery( spinner ).hide();
+            checkoutBtn.removeAttribute( 'disabled' );
             clearInterval( animation );
         });
     });
-
-
 }
