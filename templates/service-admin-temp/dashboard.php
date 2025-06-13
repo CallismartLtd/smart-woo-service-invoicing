5b9ba755b7aec6dd10d067dcd6d701e6bd0dab62
Callismart Tech <admin@callismart.com.ng>
2025-06-13 12:59:25 +0100

    Update dashboard.php

--- templates/service-admin-temp/dashboard.php
+++ templates/service-admin-temp/dashboard.php
@@ -11,35 +11,38 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <div class="sw-admin-dashboard" id="sw-admin-dash">
     <div class="sw-admin-dash-header">
-        <!-- Smart Woo Info -->
-        <div class="sw-admin-dash-info">
-        <img src="<?php echo esc_url( SMARTWOO_DIR_URL . '/assets/images/smart-woo-official-icon.png' ) ?>" alt="Smart Woo Icon">
-        <h1><?php echo wp_kses_post( apply_filters( 'smartwoo_dashboard_name', 'Smart Woo' ) );?></h1>
-        <em><?php echo esc_html( apply_filters( 'smartwoo_dashboard_version', SMARTWOO_VER ) );?></em>
-        </div>
-
-        <!-- Navigation buttons -->
-        <div class="sw-admin-dash-nav">
-            <a id="dashboardBtn">Dashboard</a>
-            <a id="dashAddNew">Add New</a>
-            <a id="dashOrderBtn">Orders</a>
-            <a id="dashInvoicesBtn">Invoices</a>
-            <a id="dashProductBtn">Products</a>
-            <a id="dashSettingsBtn">Settings</a>
-        </div>
-        <div class="sw-admin-menu-icon">
-	        <span class="dashicons dashicons-menu"></span>
-	    </div>
-
-        <?php if( ! class_exists( 'SmartWooPro', false ) ):?>
-            <div class="sw-upgrade-to-pro">
-                <a><?php echo esc_html( apply_filters( 'smartwoo_dash_pro_button_text', __( 'Activate Pro Features', 'smart-woo-service-invoicing' ) ) );?></a>
+        <div class="sw-admin-header-content">
+            <!-- Smart Woo Info -->
+            <div class="sw-admin-dash-info">
+                <img src="<?php echo esc_url( SMARTWOO_DIR_URL . '/assets/images/smart-woo-official-icon.png' ) ?>" alt="Smart Woo Icon">
+                <h1><?php echo wp_kses_post( apply_filters( 'smartwoo_dashboard_name', 'Smart Woo' ) );?></h1>
+                <em><?php echo esc_html( apply_filters( 'smartwoo_dashboard_version', SMARTWOO_VER ) );?></em>
             </div>
-        <?php endif;?>
 
+            <!-- Navigation buttons -->
+            <div class="sw-admin-dash-nav">
+                <ul>
+                    <li id="dashboardBtn">Dashboard</li>
+                    <li id="dashAddNew">Add New</li>
+                    <li id="dashOrderBtn">Orders</li>
+                    <li id="dashInvoicesBtn">Invoices</li>
+                    <li id="dashProductBtn">Products</li>
+                    <li id="dashSettingsBtn">Settings</li>
+                </ul>
+            </div>
+            <div class="sw-admin-menu-icon">
+                <span class="dashicons dashicons-menu"></span>
+            </div>
+
+            <?php if( ! class_exists( 'SmartWooPro', false ) ):?>
+                <div class="sw-upgrade-to-pro">
+                    <a><?php echo esc_html( apply_filters( 'smartwoo_dash_pro_button_text', __( 'Activate Pro Features', 'smart-woo-service-invoicing' ) ) );?></a>
+                </div>
+            <?php endif;?>
+        </div>
     </div>
 
-    <div id="swloader"><?php esc_html_e( 'Just a moment', 'smart-woo-service-invoicing' ); ?></div>
+    <div id="swloader" style="background-color: #f1f1f100"></div>
     <div class="sw-admin-dash-body">
         <?php do_action( 'smartwoo_admin_header' ) ?>
         <div class="sw-search-container">
@@ -53,6 +56,7 @@ defined( 'ABSPATH' ) || exit;
                 <div class="sw-skeleton sw-skeleton-text"></div>
                 <div class="sw-skeleton sw-skeleton-text"></div>
             </div>
+            
         </div>
 
     </div>
