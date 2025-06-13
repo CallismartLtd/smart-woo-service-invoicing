e45a3ed66abc2c5b9ca14108797f4b93be8a8e1d
Callismart Tech <admin@callismart.com.ng>
2025-06-13 12:59:34 +0100

    Create class-orders-controller.php

---
+++ includes/admin/class-orders-controller.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * The Smart Woo Order Controller file
+ * 
+ * @author Callistus
+ * @package SmartWoo\classes
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Smart Woo Order controller class handles all admin functionalities for
+ * a Smart Woo Order management.
+ */
+class SmartWoo_Orders_Controller {
+
+    /**
+     * Admin Service Order page contrlloer
+     */
+    public static function menu_controller() {
+        $section = isset( $_GET['section'] ) ? sanitize_key( wp_unslash( $_GET['section'] ) ) : '';
+        switch( $section ) {
+            case 'process-order':
+                self::process_order_form();
+                break;
+            default :
+            self::dashboard();
+        }
+    }
+
+    /**
+     * The Dashboard page
+     */
+    private static function dashboard() {
+        $paged      = isset( $_GET['paged'] ) ? absint( $_GET['paged'] ) : 1;
+        $limit      = isset( $_GET['limit'] ) ? absint( $_GET['limit'] ) : 20;
+        $all_orders = SmartWoo_Order::count_all();
+        $total      = ceil( $all_orders / $limit );
+        $prev       = $paged > 1 ? $paged - 1 : 1;
+        $next       = $paged < $total ? $paged + 1 : $total;
+        $orders     = SmartWoo_Order::get_all( $paged, $limit );
+        $btn_text   = ( function( $status, $order_id ) {
+            if ( 'awaiting processing' === $status ) {
+                return '<a href="' . esc_url( admin_url( 'admin.php?page=sw-service-orders&section=process-order&order_id=' . $order_id ) ) . '"><button class="sw-icon-button-admin" title="' .__( 'Process Now', 'smart-woo-service-invoicing' ). '"><span class="dashicons dashicons-ellipsis"></span></button></a>';
+            }elseif( 'processed' === $status ) {
+                return '<button class="sw-icon-button-admin sw-not-allowed" title="' . __( 'Order has been processed', 'smart-woo-service-invoicing' ) . '"><span style="color:#f0a607" class="dashicons dashicons-cloud-saved"></span></button>';
+            }elseif( 'awaiting payment' === $status ) {
+                return '<button class="sw-icon-button-admin sw-not-allowed" title="' . __( 'The order has been received, but no payment has been made', 'smart-woo-service-invoicing' ) . '"><span style="color: #2217ee" class="dashicons dashicons-info"></span></button>';
+            } else {
+                return '<button class="sw-icon-button-admin sw-not-allowed" title="' . __( 'Cannot be processed', 'smart-woo-service-invoicing' ) . '"><span style="color: #2217ee" class="dashicons dashicons-info"></span></button>';
+            }
+        });
+        include_once SMARTWOO_PATH . 'templates/order-admin/dashboard.php';
+    }
+
+    /**
+     * The order processing form page
+     */
+    private static function process_order_form() {
+        wp_enqueue_script( 'smartwoo-jquery-timepicker' );
+        wp_enqueue_media();
+        smartwoo_set_document_title( 'Process Orders' );
+        $order_id   = isset( $_GET['order_id'] ) ? absint( $_GET['order_id'] ) : 0; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+        $order      = SmartWoo_Order::get_order( $order_id );
+
+        if ( $order ) {
+            $product            = $order->get_product();
+            $GLOBALS['product']	= $product;
+
+            $has_asset          = $product->is_downloadable();
+            $product_name       = $product ? $product->get_name() : 'N/A';
+            $service_name       = $order->get_service_name();
+            $product_id         = $product ? $product->get_id() : '';
+            $service_url        = $order->get_service_url();
+            $user_full_name     = $order->get_user() ? $order->get_user()->get_billing_first_name() . ' ' . $order->get_user()->get_billing_last_name() : '';
+            $user_id            = $order->get_user() ? $order->get_user()->get_id() : 0;
+            $start_date         = $order->get_date_paid( 'date_format' );
+            $billing_cycle     	= $order->get_billing_cycle();
+            $next_payment_date 	= '';
+            $end_date          	= '';
+            $status            	= '';
+            $user               = get_userdata( $user_id );
+            $downloadables      = $product->get_smartwoo_downloads();
+        
+            // Set next payment date and end date based on billing cycle.
+            switch ( $billing_cycle ) {
+                case 'Monthly':
+                    $end_date          = date_i18n( 'Y-m-d', strtotime( $start_date . ' +1 month' ) );
+                    $next_payment_date = date_i18n( 'Y-m-d', strtotime( $end_date . ' -7 days' ) );
+                    break;
+
+                case 'Quarterly':
+                    $end_date          = date_i18n( 'Y-m-d', strtotime( $start_date . ' +3 months' ) );
+                    $next_payment_date = date_i18n( 'Y-m-d', strtotime( $end_date . ' -7 days' ) );
+                    break;
+
+                case 'Six Monthly':
+                    $end_date          = date_i18n( 'Y-m-d', strtotime( $start_date . ' +6 months' ) );
+                    $next_payment_date = date_i18n( 'Y-m-d', strtotime( $end_date . ' -7 days' ) );
+                    break;
+
+                case 'Yearly':
+                    $end_date          = date_i18n( 'Y-m-d', strtotime( $start_date . ' +1 year' ) );
+                    $next_payment_date = date_i18n( 'Y-m-d', strtotime( $end_date . ' -7 days' ) );
+                    break;
+                default:
+                    break;
+            }
+
+            $status_options 	= array(
+                ''					=> esc_html__( 'Auto Calculate', 'smart-woo-service-invoicing' ),
+                'Pending'			=> esc_html__( 'Pending', 'smart-woo-service-invoicing' ),
+                'Active (NR)'		=> esc_html__( 'Active (NR)', 'smart-woo-service-invoicing' ),
+                'Suspended'			=> esc_html__( 'Suspended', 'smart-woo-service-invoicing' ),
+                'Due for Renewal'	=> esc_html__( 'Due for Renewal', 'smart-woo-service-invoicing' ),
+                'Expired'			=> esc_html__( 'Expired', 'smart-woo-service-invoicing' ),
+            );
+        }
+        
+        include_once SMARTWOO_PATH . 'templates/order-admin/new-service-order-form.php';
+    }
+}
