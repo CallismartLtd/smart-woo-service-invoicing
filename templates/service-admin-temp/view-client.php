4d21c85c558618216dbf34e21b4fb33fa4d47c50
Callismart Tech <admin@callismart.com.ng>
2025-06-14 04:13:02 +0100

    Update view-client.php

--- templates/service-admin-temp/view-client.php
+++ templates/service-admin-temp/view-client.php
@@ -9,8 +9,6 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 <div class="sw-admin-view-details">
-    <?php echo wp_kses_post( smartwoo_sub_menu_nav( $tabs, 'Client','sw-admin', $tab, 'service_id=' . $service_id . '&tab' ) ); ?>
-
     <?php if ( ! $service ) : ?>
         <?php echo wp_kses_post( smartwoo_notice( 'Invalid or deleted service <a href="' . admin_url( 'admin.php?page=sw-admin' ) . '">back</a>' ) ); ?>
     <?php else : ?>
