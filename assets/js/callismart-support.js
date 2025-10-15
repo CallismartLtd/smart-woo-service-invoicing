43e1395822db0bda4bebacc7f573f83e9b5a529b
Callismart Tech <admin@callismart.com.ng>
2025-10-15 16:11:54 +0100

    Create callismart-support.js

---
+++ assets/js/callismart-support.js
@@ -0,0 +1,16 @@
+/**
+ * Callismart Tech Support Script
+ *
+ * @package SmartWoo
+ * @subpackage JS
+ * @since 1.0.0
+ */
+
+class CallismartSupport {
+    
+}
+
+// Global execution wrapper
+document.addEventListener('DOMContentLoaded', function() {
+
+});
