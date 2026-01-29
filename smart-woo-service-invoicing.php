fae273ad3cbfad17cbc8831241aafcecffec1a86
CallismartLtd <admin@callismart.com.ng>
2026-01-29 17:30:19 +0100

    Bump version to 2.5.3 in plugin header and version constants

--- smart-woo-service-invoicing.php
+++ smart-woo-service-invoicing.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Smart Woo Service Invoicing
  * Description: More than just a subscription plugin.
- * Version: 2.5.2
+ * Version: 2.5.3
  * Author: Callistus Nwachukwu
  * Author URI: https://callismart.com.ng/callistus
  * Plugin URI: https://callismart.com.ng/smart-woo-service-invoicing
@@ -44,11 +44,11 @@ if ( ! defined( 'SMARTWOO_DIR_URL' ) ) {
 
 // Define the Smart Woo versions.
 if ( ! defined( 'SMARTWOO_VER' ) ) {
-	define( 'SMARTWOO_VER', '2.5.1' );
+	define( 'SMARTWOO_VER', '2.5.3' );
 }
 
 if ( ! defined( 'SMARTWOO_DB_VER' ) ) {
-	define( 'SMARTWOO_DB_VER', '2.5.1' );
+	define( 'SMARTWOO_DB_VER', '2.5.3' );
 }
 
 // Load core and config files.
